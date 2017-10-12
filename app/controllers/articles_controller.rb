5682163254effb233892d354ef4ab80fe6b6799f
mohsenjalalian <jalalianmohsen@gmail.com>
2017-10-12 08:34:49 +0330

    index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
   end
 
   def create
     @article = Article.new(article_params)
-
     @article.save
     redirect_to @article
   end
